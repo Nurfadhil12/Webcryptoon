bba4fd3af114cf62bc116ada969408fdad7d1d0e
Nurfadhil12 <nuurrff@gmail.com>
2021-10-28 22:04:06 +0700

    design by ariq

---
+++ Index.js
@@ -0,0 +1,50 @@
+class Table {
+    constructor(init) {
+      this.init = init;
+    }
+  
+    createHeader(data) {
+      let open = "<thead><tr>";
+      let close = "</tr></thead>";
+      data.forEach((d) => {
+        open += `<th>${d}</th>`;
+      });
+  
+      return open + close;
+    }
+  
+    createBody(data) {
+      let open = "<tbody>";
+      let close = "</tbody>";
+  
+      data.forEach((d) => {
+        open += `
+          <tr>
+            <td>${d[0]}</td>
+            <td>${d[1]}</td>
+          </tr>
+        `;
+      });
+  
+      return open + close;
+    }
+  
+    render(element) {
+      let table =
+        "<table class='table table-hover'>" +
+        this.createHeader(this.init.columns) +
+        this.createBody(this.init.data) +
+        "</table>";
+      element.innerHTML = table;
+    }
+  }
+
+  const grid = new Grid({
+    columns: ['Name', 'Email', 'Phone Number'],
+    data: [
+      ['John', 'john@example.com', '(353) 01 222 3333'],
+      ['Mark', 'mark@gmail.com',   '(01) 22 888 4444']
+    ],
+    resizable: true,
+    sort: true
+  });
